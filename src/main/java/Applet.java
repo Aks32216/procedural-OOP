680b9aafaf3cbce18ce87e9ed4f6108c0383cb46
Aks32216 <amishsingh568@gmail.com>
2024-05-04 09:19:38 +0530

    corrected naming and introduced array in position of list

--- src/main/java/Applet.java
+++ src/main/java/Applet.java
@@ -3,14 +3,15 @@ import processing.core.PApplet;
 import java.util.Arrays;
 import java.util.List;
 
-public class FourBalls extends PApplet {
+public class Applet extends PApplet {
     public static final int WIDTH = 1800;
     public static final int HEIGHT = 1000;
     public static final int DIAMETER = 100;
-    List<Circle> balls;
+    public int numberOfBalls = 4;
+    Balls[] balls;
 
     public static void main(String[] args) {
-        PApplet.main("FourBalls",args);
+        PApplet.main("Applet",args);
     }
 
     @Override
@@ -21,18 +22,16 @@ public class FourBalls extends PApplet {
 
     @Override
     public void setup() {
-        Circle c1=new Circle(1,height/5,0, DIAMETER);
-        Circle c2=new Circle(2,height/5,0,DIAMETER);
-        Circle c3=new Circle(3,height/5,0,DIAMETER);
-        Circle c4=new Circle(4,height/5,0,DIAMETER);
-
-        balls=Arrays.asList(c1,c2,c3,c4);
+        balls=new Balls[numberOfBalls];
+        for(int i=1;i<=numberOfBalls;++i){
+            balls[i-1]=new Balls(0,i*(HEIGHT/5),i,DIAMETER);
+        }
     }
 
     @Override
     public void draw() {
-        for(Circle ball:balls){
-            ellipse(ball.getSpeed(),ball.getHeight(),ball.getDiameter(),ball.getDiameter());
+        for(Balls ball:balls){
+            ellipse(ball.getXPosition(),ball.getYPosition(),ball.getDiameter(),ball.getDiameter());
             ball.updateSpeed();
         }
     }
---
+++ src/main/java/Balls.java
@@ -0,0 +1,30 @@
+public class Balls {
+    private int xPosition;
+    private int yPosition;
+    private int speedRate;
+    private int diameter;
+
+    public Balls(int xPosition,int yPosition,int speedRate,int diameter){
+        this.xPosition=xPosition;
+        this.yPosition=yPosition;
+        this.speedRate=speedRate;
+        this.diameter=diameter;
+    }
+
+
+    public int getSpeedRate() {
+        return speedRate;
+    }
+
+    public int getYPosition() {
+        return yPosition;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public int getXPosition() { return xPosition; }
+
+    public void updateSpeed(){ this.xPosition=getSpeedRate()+getXPosition(); }
+}
